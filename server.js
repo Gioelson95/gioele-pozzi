380bddc00bb36f262585badd656f3ae67006871b
Gioelson95 <10454628@polimi.it>
2018-02-13 23:57:42 +0100

    return

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
 // Import the library
-//const server = require('server');
-const app = express();
-
-let serverPort = process.env.PORT || 5000;
-
-// start server
-app.listen(serverPort, function () {
-	console.log("Your app is ready at port " + serverPort);
-});
+const server = require('server');
 
 // Launch the server to always answer "Hello world"
-//server(ctx => 'Hello world!');
+server(ctx => 'Hello world!');
