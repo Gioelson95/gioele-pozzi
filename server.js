5286e7c738d81c8cb0b480850fb56e94b96d2725
Gioelson95 <10454628@polimi.it>
2018-02-14 00:30:19 +0100

    tr

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 // Import the library
-const server = require('server');
-var http = require('http');
+//const server = require('server');
 
 // Launch the server to always answer "Hello world"
-server(ctx => 'Hello world!');
+//server(ctx => 'Hello world!');
+
+var http = require('http');
+var fs = require("fs");
+
+http.createServer(function (request, response) {
+	fs.readFile("index.html", function (err, data) {
+		response.writeHead(200, {
+			'Content-Type': 'text/html'
+		});
+		response.write(data);
+		response.end();
+	});
+}).listen(3000);
