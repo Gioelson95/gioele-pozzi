3b0af87df211281106b9a48683ebf883174e618d
Gioelson95 <10454628@polimi.it>
2018-02-13 23:55:56 +0100

    try to function heroku

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 // Import the library
-const server = require('server');
+//const server = require('server');
+const app = express();
+
+let serverPort = process.env.PORT || 5000;
+
+// start server
+app.listen(serverPort, function () {
+	console.log("Your app is ready at port " + serverPort);
+});
 
 // Launch the server to always answer "Hello world"
-server(ctx => 'Hello world!');
+//server(ctx => 'Hello world!');
