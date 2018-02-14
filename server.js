298feb0812692691178c832ed8b103fe24dbb24d
Gioelson95 <10454628@polimi.it>
2018-02-14 20:23:33 +0100

    try on the server side

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 // Import the library
-const server = require('server');
+//const server = require('server');
 
 // Launch the server to always answer "Hello world"
-server(ctx => 'Hello world!');
+//server(ctx => 'Hello world!');
+
+
+
+// Dichiazione framework
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const sqlDbfactory = require("knex");
+const _ = require("lodash");
+const process = require("process");
+
+// Dichairazione variabili
+let serverPort = process.env.PORT || 5000;
+
+// start server
+app.listen(serverPort, function () {
+	console.log("Your app is ready at port " + serverPort);
+});
