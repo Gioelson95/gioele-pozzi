3d8684b1223e22983ddeb64b34cd79225d89dcc5
Gioelson95 <10454628@polimi.it>
2018-02-15 19:54:42 +0100

    tryyyy

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 // Import the library
-const server = require('server');
+//const server = require('server');
 
 // Launch the server to always answer "Hello world"
-server(ctx => 'Hello world!');
+//server(ctx => 'Hello world!');
+
+
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+	//Open a file on the server and return it's content:
+	fs.readFile('index.html', function (err, data) {
+		res.writeHead(200, {
+			'Content-Type': 'text/html'
+		});
+		res.write(data);
+		return res.end();
+	});
+}).listen(5000);
+
+console.log('run server on port 5000');
 
