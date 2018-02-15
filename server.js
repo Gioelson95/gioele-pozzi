bde75ee60fb44e72f71495d5d0ef0148e2d8d8f2
Gioelson95 <10454628@polimi.it>
2018-02-15 20:23:45 +0100

    asdf

--- server.js
+++ server.js
@@ -4,19 +4,12 @@
 // Launch the server to always answer "Hello world"
 //server(ctx => 'Hello world!');
 
-
 var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-	//Open a file on the server and return it's content:
-	fs.readFile('index.html', function (err, data) {
-		res.writeHead(200, {
-			'Content-Type': 'text/html'
-		});
-		res.write(data);
-		return res.end();
+http.createServer(function (request, response) {
+	response.writeHead(200, {
+		'Context-Type': 'text/plain',
 	});
+	response.end('Hello World!');
 }).listen(5000);
 
-console.log('run server on port 5000');
-
+console.log('server running at port 5000');
