9147aaf6a7fa8543cdb42065b6f98193daae17d6
Gioelson95 <10454628@polimi.it>
2018-02-15 21:49:15 +0100

    all ok

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-// Import the library
-//const server = require('server');
-
-// Launch the server to always answer "Hello world"
-//server(ctx => 'Hello world!');
-
-
-// SERVER DA HYP
 const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname));
 
-
-// Dichairazione variabili
 let serverPort = process.env.PORT || 5000;
-
 
 // start server
 app.listen(serverPort, function () {
